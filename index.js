148ba4050687ce982065a08227745e073e84f28c
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-04-03 19:09:56 +0300

    Update js file

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const searchBtn = document.getElementById('searchBtn');
+searchBtn.addEventListener('click', () => {
+  const postId = document.getElementById('postId').value;
+
+  if(postId >= 1 && postId <= 100) {
+    getPost(postId)
+    .then(post => {
+      const postBlock = document.getElementById('postBlock');
+      postBlock.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        <button id="commentsBtn">Коменты</button>
+      `;
+      
+      const commentsBtn = document.getElementById('commentsBtn');
+      commentsBtn.addEventListener('click', () => {
+        getComments(postId)
+          .then(comments => {
+            const commentsBlock = document.createElement('div');
+            commentsBlock.innerHTML = `
+            <h3>Коментарии:</3>
+            <ul>
+              ${comments.map(comment => `<li>${comment.name}: ${comment.body}</li>`).join('')}
+            </ul>
+            `;
+            postBlock.appendChild(commentsBlock);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  } else {
+    alert ('Пожалуйста, введите ID в диапазоне от 1 до 100.');
+  }
+});
+
+function getPost(postId) {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  .then(response => {
+    if(!response.ok) {
+      throw new Error('Случилась ошибка. Попробуйте еще раз.');
+    }
+    return response.json();
+  });
+}
+
+function getComments(postId) {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Случилась ошибка. Попробуйте еще раз.');
+      }
+      return response.json();
+    });
+}
